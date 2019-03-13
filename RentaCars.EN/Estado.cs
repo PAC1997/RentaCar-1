576a4c93bcda8d9e64700a3ca56381355b1b34a0
pedri <pedri@DESKTOP-355HPV5>
2019-03-13 09:55:07 -0600

    Agregar archivos de proyecto.

---
+++ RentaCars.EN/Estado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Agregar
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentaCars.EN
+{
+  public class Estado
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Id")]
+        public int EstadoID { get; set; }
+        [Required(ErrorMessage ="Campo no puede estar vacio")]
+        [RegularExpression("^[A-Za-z ]+$", ErrorMessage = "Solo letras")]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+        
+
+        public virtual ICollection<Usuario> Usuario { get; set; }
+        public virtual ICollection<TipoUsuario> TipoUsuario { get; set; }
+        public virtual ICollection<Vehiculo> Vehiculo { get; set; }
+        public virtual ICollection<Reserva> Reserva { get; set; }
+        public virtual ICollection<Renta> Renta { get; set; }
+    }
+}
---
+++ RentaCars.EN/Renta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Agregar
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentaCars.EN
+{
+  public class Renta
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Id")]
+        public int RentaID { get; set; }
+        [Display(Name = "Usuario id")]
+        [Required(ErrorMessage = "Campo no puede estar vacio")]
+        public int UsuarioID { get; set; }
+        [Display(Name = "Reserva id")]
+        [Required(ErrorMessage = "Campo no puede estar vacio")]
+        public int ReservaID { get; set; }
+        [Display(Name = "Estado id")]
+        public int EstadoID { get; set; }
+
+        public virtual Usuario Usuario { get; set;}
+        public virtual Reserva Reserva { get; set; }
+        public virtual Estado Estado { get; set; }
+
+
+    }
+}
